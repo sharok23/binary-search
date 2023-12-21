73d04d41ef189877cf5e3bdb23102d35a14c3579
Sharok N <Sharukhahamed23@gmail.com>
2023-12-21 13:38:21 +0530

    added execution time

--- src/main/java/com/edstem/binarysearch/contract/response/BinarySearchResponse.java
+++ src/main/java/com/edstem/binarysearch/contract/response/BinarySearchResponse.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class BinarySearchResponse {
     private long indexValue;
+    private long executionTime;
 }
--- src/main/java/com/edstem/binarysearch/service/BinarySearchService.java
+++ src/main/java/com/edstem/binarysearch/service/BinarySearchService.java
@@ -5,6 +5,7 @@ import com.edstem.binarysearch.contract.response.BinarySearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -13,19 +14,24 @@ import java.util.NoSuchElementException;
 public class BinarySearchService {
 
     public BinarySearchResponse binarySearch(BinarySearchRequest request, long value) {
+        long startTime = System.nanoTime();
         List<Integer> array = request.getNumbers();
         int left = 0;
         int right = array.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
             if (value == array.get(mid)) {
-                return new BinarySearchResponse(mid);
+                long endTime = System.nanoTime();
+                long executionTime = endTime - startTime;
+                return new BinarySearchResponse(mid, executionTime);
             } else if (array.get(mid) < value) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        throw new NoSuchElementException("Value not found in array");
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
+        throw new NoSuchElementException("Value not found in array. Execution time: " + executionTime);
     }
 }
